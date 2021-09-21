8f5e324f6502b89f9d9fdfe3f2198f15b410d5d8
Bordignon Schuvantek Nunes <robu@gft.com>
2021-09-21 16:24:34 -0300

    ajustando config mail chimp

--- back-end/src/services/MailChimpTest.js
+++ back-end/src/services/MailChimpTest.js
@@ -1,27 +1,29 @@
-const mailchimp = require("@mailchimp/mailchimp_transactional")("e6ef5286ecd75fe98b2fbb6a7b3cde05-us5");
+const mailchimp = require("@mailchimp/mailchimp_transactional")(
+  "6OxWlWa79BHIK5BrXHsnkw"
+);
+
+// mailchimp.setConfig({
+//   apiKey: "2d645aa617eead1d90ada5e0cacde070-us5",
+//   server: "us5",
+// });
 
 module.exports = {
+  async run() {
+    const message = {
+      from_email: "styflerbbs@gmail.com",
+      subject: "Hello world",
+      text: "Welcome to Mailchimp Transactional!",
+      to: [
+        {
+          email: "crytektv@gmail.com",
+          type: "to",
+        },
+      ],
+    };
 
-    async run() {
-
-
-        const message = {
-            from_email: "styflerbbs@gmail.com",
-            subject: "Hello world",
-            text: "Welcome to Mailchimp Transactional!",
-            to: [
-                {
-                    email: "crytektv@gmail.com",
-                    type: "to"
-                }
-            ]
-        };
-
-
-        const response = await mailchimp.messages.send({
-            message
-        });
-        console.log(response);
-    }
-
-}
+    const response = await mailchimp.messages.send({
+      message,
+    });
+    console.log(response);
+  },
+};
